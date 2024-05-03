09e6e86e56dde11c78ed5f2a5f06b6ea9a75fa49
Cramzzzberry <cramzzzberry@gmail.com>
2024-05-03 23:33:43 +0800

    Refactor: Removed logs

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -85,11 +85,9 @@ router.post("/login", async (req, res) => {
                       section: user.section,
                     });
                   })
-                  .catch((err) => {
-                    console.log(err);
-                    console.log(req.body.password + " - " + user.password);
-                    res.status(500).send("Internal server error");
-                  });
+                  .catch((err) =>
+                    res.status(500).send("Internal server error")
+                  );
               } else {
                 res.status(401).send("Account not yet approved");
               }
